febc68e03df07369c4e806bc264b35c396955928
Nguyễn Hải Nam <59411728+NguyenHaiNam24082000@users.noreply.github.com>
2020-08-26 23:25:06 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const bgCanvas= document.getElementById('bg-canvas');
+const buttonPlayBefore= document.getElementById('play-btn-before');
+const buttonPlayAfter= document.getElementById('play-btn-after');
+const urlImage = bgCanvas.getAttribute('data-bg-image');
+function playGame()
+{
+    this.addEventListener('click',function()
+    {
+        buttonPlayAfter.style.animation="click-button 2s";
+    });
+    setTimeout(function()
+    {
+        bgCanvas.style.background="url("+ urlImage +"-"+(Math.floor(Math.random()*7)+1)+".png)";
+        buttonPlayAfter.style.display="none";
+        buttonPlayBefore.style.display="none";
+    },2050);
+}
