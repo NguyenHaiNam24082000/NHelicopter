01d6f5765d3f9dcfb2139da58520e4f3aa29902a
Nguyễn Hải Nam <59411728+NguyenHaiNam24082000@users.noreply.github.com>
2020-08-26 23:34:53 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const buttonPlayAfter= document.getElementById('play-btn-after');
 const urlImage = bgCanvas.getAttribute('data-bg-image');
 function playGame()
 {
+    console.log("url("+ urlImage +"-"+(Math.floor(Math.random()*7)+1)+".png)");
     this.addEventListener('click',function()
     {
         buttonPlayAfter.style.animation="click-button 2s";
