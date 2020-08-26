a6a9791896d625df9580e03664a13a85d07de513
Nguyễn Hải Nam <59411728+NguyenHaiNam24082000@users.noreply.github.com>
2020-08-26 23:48:20 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,25 @@
 const bgCanvas= document.getElementById('bg-canvas');
 const buttonPlayBefore= document.getElementById('play-btn-before');
 const buttonPlayAfter= document.getElementById('play-btn-after');
-const urlImage = bgCanvas.getAttribute('data-bg-image');
+const urlImage = {
+    bg1:    "url(../images/background-1.png)",
+    bg2:    "url(../images/background-1.png)",
+    bg3:    "url(../images/background-1.png)",
+    bg4:    "url(../images/background-1.png)",
+    bg5:    "url(../images/background-1.png)",
+    bg6:    "url(../images/background-1.png)",
+    bg7:    "url(../images/background-1.png)",
+};
 function playGame()
 {
-    console.log("url("+ urlImage +"-"+(Math.floor(Math.random()*7)+1)+".png)");
+    console.log(urlImage["bg"+Math.floor(Math.random()*7+1)]);
     this.addEventListener('click',function()
     {
         buttonPlayAfter.style.animation="click-button 2s";
     });
     setTimeout(function()
     {
-        bgCanvas.style.background="url("+ urlImage +"-"+(Math.floor(Math.random()*7)+1)+".png)";
+        bgCanvas.style.background=urlImage["bg"+Math.floor(Math.random()*7+1)];
         buttonPlayAfter.style.display="none";
         buttonPlayBefore.style.display="none";
     },2050);
