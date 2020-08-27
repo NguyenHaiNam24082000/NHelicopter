1b84abcd623d999dd669af7e84e1123c7587b8ca
Nguyễn Hải Nam <59411728+NguyenHaiNam24082000@users.noreply.github.com>
2020-08-27 12:52:13 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,13 +3,13 @@ const buttonPlayBefore= document.getElementById('play-btn-before');
 const buttonPlayAfter= document.getElementById('play-btn-after');
 let randomBg;
 const urlImage = {
-    bg1:    "url(../images/background-1.png)",
-    bg2:    "url(../images/background-2.png)",
-    bg3:    "url(../images/background-3.png)",
-    bg4:    "url(../images/background-4.png)",
-    bg5:    "url(../images/background-5.png)",
-    bg6:    "url(../images/background-6.png)",
-    bg7:    "url(../images/background-7.png)",
+    bg1:    "url(.././images/background-1.png)",
+    bg2:    "url(.././images/background-2.png)",
+    bg3:    "url(.././images/background-3.png)",
+    bg4:    "url(.././images/background-4.png)",
+    bg5:    "url(.././images/background-5.png)",
+    bg6:    "url(.././images/background-6.png)",
+    bg7:    "url(.././images/background-7.png)",
 };
 function playGame()
 {
