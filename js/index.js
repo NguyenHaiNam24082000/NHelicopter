6f682a6f8cb1960ad29f8ed6f4934ee982baa80e
Nguyễn Hải Nam <59411728+NguyenHaiNam24082000@users.noreply.github.com>
2020-08-27 12:47:47 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,29 @@
 const bgCanvas= document.getElementById('bg-canvas');
 const buttonPlayBefore= document.getElementById('play-btn-before');
 const buttonPlayAfter= document.getElementById('play-btn-after');
+let randomBg;
 const urlImage = {
     bg1:    "url(../images/background-1.png)",
-    bg2:    "url(../images/background-1.png)",
-    bg3:    "url(../images/background-1.png)",
-    bg4:    "url(../images/background-1.png)",
-    bg5:    "url(../images/background-1.png)",
-    bg6:    "url(../images/background-1.png)",
-    bg7:    "url(../images/background-1.png)",
+    bg2:    "url(../images/background-2.png)",
+    bg3:    "url(../images/background-3.png)",
+    bg4:    "url(../images/background-4.png)",
+    bg5:    "url(../images/background-5.png)",
+    bg6:    "url(../images/background-6.png)",
+    bg7:    "url(../images/background-7.png)",
 };
 function playGame()
 {
-    console.log(urlImage["bg"+Math.floor(Math.random()*7+1)]);
+    randomBg=Math.floor(Math.random()*7+1);
+    console.log(randomBg);
     this.addEventListener('click',function()
     {
-        buttonPlayAfter.style.animation="click-button 2s";
+        buttonPlayAfter.style.animation="click-button 1s linear";
     });
+    bgCanvas.style.background="rgb(91, 188, 147),"+ urlImage["bg"+randomBg];
     setTimeout(function()
     {
-        bgCanvas.style.background=urlImage["bg"+Math.floor(Math.random()*7+1)];
+        bgCanvas.style.background=urlImage["bg"+randomBg];
         buttonPlayAfter.style.display="none";
         buttonPlayBefore.style.display="none";
-    },2050);
+    },1050);
 }
